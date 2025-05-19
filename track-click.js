eeab7cbdc4d40766499c66906f0d7e03102c60e7
yongzu98 <yongzu3355@gmail.com>
2025-05-19 18:11:39 +0900

    Update track-click.js

--- track-click.js
+++ track-click.js
@@ -1,5 +1,5 @@
 function trackClick(menuName) {
-  fetch("https://script.google.com/macros/s/여기에_당신의_웹앱_URL/exec", {
+  fetch("https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbz1EuI2R3g8okHbrsW4n6JpH8EZ_H189O8z02zZEh61YAAEVelb4OIwwKpL2RB_X5iN6w/exec/exec", {
     method: "POST",
     body: JSON.stringify({
       menu: menuName,
