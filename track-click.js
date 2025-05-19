fa033d49619e64a166abfe8d6bd12e7cee5782eb
yongzu98 <yongzu3355@gmail.com>
2025-05-19 22:12:56 +0900

    Update track-click.js

--- track-click.js
+++ track-click.js
@@ -1,5 +1,5 @@
 function trackClick(menuName) {
-  fetch("https://script.google.com/macros/s/AKfycbwW0JaG6H1FBOs92RcaR3TVSrk_kV-V4tmOjIpAbdh_kq_jdSIB6rnhzQ0Ags39BALP/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbzkHWbAcUq2OyIx0hWTnf-U-wIVFY1o05BIcE08O8SDy62FKUiIpDyv47BBKNlPDa0D/exec", { // YOUR_WEB_APP_URL 를 실제 웹 앱 URL로 변경
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -11,5 +11,3 @@ function trackClick(menuName) {
     })
   });
 }
-
-
